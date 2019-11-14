13d7aa82d8b323721727f367e56e293a4592d4f4
Pedro Alcântara <pfalcantara@stefanini.com>
2019-11-14 13:11:48 -0300

    :tada: initial commit github

--- App.jsx
+++ App.jsx
@@ -1,30 +1,33 @@
-import React, { useState } from "react";
-import { StyleSheet, View, FlatList, Button } from "react-native";
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
+import React, { useState } from 'react'
+import { StyleSheet, View, FlatList, Button } from 'react-native'
+import GoalItem from './components/GoalItem'
+import GoalInput from './components/GoalInput'
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
+  const [courseGoals, setCourseGoals] = useState([])
 
-  const [isAddMode, setIsAddMode] = useState(false);
+  const [isAddMode, setIsAddMode] = useState(false)
 
   const addGoalHandler = goalTitle => {
+    if (goalTitle === 0) {
+      return
+    }
     setCourseGoals(currentGoals => [
       ...courseGoals,
       { id: Math.random().toString(), value: goalTitle }
-    ]);
-    setIsAddMode(false);
-  };
+    ])
+    setIsAddMode(false)
+  }
 
   const removeGoalHandler = goalId => {
     setCourseGoals(currentGoals => {
-      return currentGoals.filter(goal => goal.id !== goalId);
-    });
-  };
+      return currentGoals.filter(goal => goal.id !== goalId)
+    })
+  }
 
   const cancelGoalAdditionHandler = () => {
-    setIsAddMode(false);
-  };
+    setIsAddMode(false)
+  }
 
   return (
     <View style={styles.screen}>
@@ -46,11 +49,11 @@ export default function App() {
         )}
       />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   screen: {
     padding: 50
   }
-});
+})
